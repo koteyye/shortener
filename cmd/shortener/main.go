524ff57308afbdd512ec687b5fe4353711202d09
koteyye <koteyye@gmail.com>
2023-09-08 21:26:52 +0300

    remove gin.SetMode(releaseMode)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/koteyye/shortener/config"
 	"github.com/koteyye/shortener/internal/app/handlers"
 	"github.com/koteyye/shortener/internal/app/service"
@@ -23,7 +22,6 @@ func main() {
 	handler := handlers.NewHandlers(services)
 
 	restServer := new(server.Server)
-	gin.SetMode(gin.ReleaseMode)
 	if err := restServer.Run(cfg.Server.Listen, handler.InitRoutes(cfg.Server.BaseURL)); err != nil {
 		log.Fatalf("Error occuped while runing Rest server :%s", err.Error())
 	}
