69e13ad75045818a5280f171bcc27c13e9474cc1
koteyye <koteyye@gmail.com>
2023-12-24 20:40:42 +0300

    test example

--- internal/app/service/service_test.go
+++ internal/app/service/service_test.go
@@ -1,21 +1,22 @@
-package service_test
+package service
 
 import (
 	"context"
 	"fmt"
 
 	"github.com/google/uuid"
-	"github.com/koteyye/shortener/internal/app/service"
+	"github.com/koteyye/shortener/config"
+	"github.com/koteyye/shortener/internal/app/storage"
 )
 
 func ExampleShortenerService_AddShortURL() {
-	var s service.ShortenerService
+	repo := storage.NewURLMap()
+	s := &ShortenerService{storage: repo, shortener: &config.Shortener{Listen: "localhost:8081"}}
+
 	ctx := context.Background()
 	userID := uuid.NewString()
 
 	url, _ := s.AddShortURL(ctx, "http://yandexpracticum.ru", userID)
 
 	fmt.Println(s.GetOriginURL(ctx, url))
-	// Output:
-	// http://localhost:8080/fewf23f23....
 }
