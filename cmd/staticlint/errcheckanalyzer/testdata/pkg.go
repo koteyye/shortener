a9c76873c24956d15a92d9110bce2332ce586528
koteyye <koteyye@gmail.com>
2024-01-14 18:58:03 +0300

    gofmt

--- cmd/staticlint/errcheckanalyzer/testdata/pkg.go
+++ cmd/staticlint/errcheckanalyzer/testdata/pkg.go
@@ -3,13 +3,13 @@ package pkg1
 import "fmt"
 
 func mulfunc(i int) (int, error) {
-    return i * 2, nil
+	return i * 2, nil
 }
 
 func errCheckFunc() {
-    // формулируем ожидания: анализатор должен находить ошибку,
-    // описанную в комментарии want
-    mulfunc(5)           // want "expression returns unchecked error"
-    res, _ := mulfunc(5) // want "assignment with unchecked error"
-    fmt.Println(res)     // want "expression returns unchecked error"
+	// формулируем ожидания: анализатор должен находить ошибку,
+	// описанную в комментарии want
+	mulfunc(5)           // want "expression returns unchecked error"
+	res, _ := mulfunc(5) // want "assignment with unchecked error"
+	fmt.Println(res)     // want "expression returns unchecked error"
 }
